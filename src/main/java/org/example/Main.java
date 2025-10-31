374adc91f966f52711892c5a02ae36ec30bc47d6
JoseG-SotoMendez <josegsm2510@gmail.com>
2025-10-30 21:49:09 -0500

    Hello world 3do commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,8 @@ package org.example;
 
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    
+    }
 }
